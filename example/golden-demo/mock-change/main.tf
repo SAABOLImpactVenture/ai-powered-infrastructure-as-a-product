e96d55a7df0b632bd04beed5da9227d3490f2b1a
GEP-V <122372616+GEP-V@users.noreply.github.com>
2025-10-09 13:02:15 -0400

    Delete example/golden-demo/mock-change/main.tf

--- example/golden-demo/mock-change/main.tf
+++
@@ -1,5 +0,0 @@
-terraform {
-  required_version = ">= 1.4.0"
-}
-variable "name" { type = string, default = "golden-demo" }
-output "demo_plan_message" { value = "Plan for ${var.name} computed successfully." }
