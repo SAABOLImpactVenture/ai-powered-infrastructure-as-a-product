c854719cb307c12133f0effa092e434d7dd54508
GEP-V <122372616+GEP-V@users.noreply.github.com>
2025-10-12 14:53:10 -0400

    feat: add minimal TF sample to exercise CI

---
+++ products/azure/example/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.114"
+    }
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "demo" {
+  name     = "rg-aiap-demo"
+  location = "eastus"
+  tags = {
+    provisioned_by = "github-actions"
+  }
+}
